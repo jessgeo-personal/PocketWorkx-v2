49ffec3da8cbeff90192ec58866c18851a6748b2
jessgeo-personal <jessgeojose@gmail.com>
2025-11-06 17:23:20 +0400

    PW1.1.2 update 6Nov2025

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Stack } from 'expo-router';
 import { StorageProvider } from '../services/storage/StorageProvider';
 import { OnboardingProvider } from '../components/onboarding/OnboardingManager';
+import ErrorBoundary from '../components/ErrorBoundary';
 
 export default function RootLayout() {
   return (
-    <StorageProvider>
-      <OnboardingProvider>
-        <Stack screenOptions={{ headerShown: false, animation: 'slide_from_right' }} />
-      </OnboardingProvider>
-    </StorageProvider>
+    <ErrorBoundary>
+      <StorageProvider>
+        <OnboardingProvider>
+          <Stack screenOptions={{ headerShown: false, animation: 'slide_from_right' }} />
+        </OnboardingProvider>
+      </StorageProvider>
+    </ErrorBoundary>
   );
 }
